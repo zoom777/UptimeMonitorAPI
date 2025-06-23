478e4bcd33ca5b6707e42f3370212c4ae9eeb523
Jonathan Aldave Mendoza <jgam777@hotmail.com>
2025-06-23 11:27:11 -0500

    endpoint de prueba

---
+++ UptimeMonitor.API/Controllers/DemoTestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UptimeMonitor.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DemoTestController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> GetDemo() =>
+            Ok("Demo testing...1.2.3.");
+    }
+}
